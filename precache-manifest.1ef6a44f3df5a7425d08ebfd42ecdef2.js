8d3ccfa13130ba594f7c6d51c6322f6038f2dc03
nahuelmedina88 <nahuelmedinasenn@gmail.com>
2020-10-01 23:22:12 +0200

    Updates

---
+++ precache-manifest.1ef6a44f3df5a7425d08ebfd42ecdef2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0290becfe4e179e04a64bb484a1805fc",
+    "url": "/ARSCurrencyConverter/index.html"
+  },
+  {
+    "revision": "f9d346c79bec444e9901",
+    "url": "/ARSCurrencyConverter/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "13b4bdaa289434a191cf",
+    "url": "/ARSCurrencyConverter/static/css/main.b824292c.chunk.css"
+  },
+  {
+    "revision": "f9d346c79bec444e9901",
+    "url": "/ARSCurrencyConverter/static/js/2.1537ea36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ARSCurrencyConverter/static/js/2.1537ea36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b4bdaa289434a191cf",
+    "url": "/ARSCurrencyConverter/static/js/main.ffe74c8f.chunk.js"
+  },
+  {
+    "revision": "d8983187b646762ae96b",
+    "url": "/ARSCurrencyConverter/static/js/runtime-main.2b45c688.js"
+  },
+  {
+    "revision": "21379b09a0bb4669c382f7724ac936c1",
+    "url": "/ARSCurrencyConverter/static/media/argentinaFlag.21379b09.png"
+  }
+]);
